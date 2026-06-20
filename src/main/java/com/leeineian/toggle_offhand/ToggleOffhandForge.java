af7a12100ac2b2b0f72901d3b1483c6ade908d4a
leeineian <86507806+leeineian@users.noreply.github.com>
2026-06-20 10:26:18 +0800

    Fix keybind registration on Forge 65.0.0

--- src/main/java/com/leeineian/toggle_offhand/ToggleOffhandForge.java
+++ src/main/java/com/leeineian/toggle_offhand/ToggleOffhandForge.java
@@ -11,52 +11,36 @@ public class ToggleOffhandForge {
         try {
             ToggleOffhand.LOGGER.info("Attempting reflective key mapping registration on Forge...");
             
-            // Get FMLModContainer and its event bus
-            Class<?> modListClass = Class.forName("net.minecraftforge.fml.ModList");
-            Object modList = modListClass.getMethod("get").invoke(null);
-            java.util.Optional<?> containerOpt = (java.util.Optional<?>) modListClass.getMethod("getModContainerById", String.class).invoke(modList, "toggle_offhand");
+            // Get RegisterKeyMappingsEvent class and its static BUS field
+            Class<?> eventClass = Class.forName("net.minecraftforge.client.event.RegisterKeyMappingsEvent");
+            Object eventBus = eventClass.getField("BUS").get(null);
             
-            if (containerOpt.isPresent()) {
-                Object container = containerOpt.get(); // FMLModContainer
-                Object eventBus = container.getClass().getMethod("getEventBus").invoke(container);
-                
-                // Get EventPriority enum and set priority to NORMAL
-                Class<?> priorityClass = Class.forName("net.minecraftforge.eventbus.api.EventPriority");
-                Object normalPriority = Enum.valueOf((Class<Enum>) priorityClass, "NORMAL");
-                
-                // Get RegisterKeyMappingsEvent class
-                Class<?> eventClass = Class.forName("net.minecraftforge.client.event.RegisterKeyMappingsEvent");
-                
-                // Set up the listener callback
-                java.util.function.Consumer<Object> handler = event -> {
-                    try {
-                        if (ToggleOffhand.keyMapping == null) {
-                            ToggleOffhand.keyMapping = new KeyMapping(
-                                    "key.toggle_offhand",
-                                    InputConstants.Type.KEYSYM,
-                                    GLFW.GLFW_KEY_UNKNOWN,
-                                    KeyMapping.Category.MISC
-                            );
-                        }
-                        event.getClass().getMethod("register", KeyMapping.class).invoke(event, ToggleOffhand.keyMapping);
-                        ToggleOffhand.LOGGER.info("Registered keybind via Forge RegisterKeyMappingsEvent reflectively!");
-                    } catch (Exception e) {
-                        ToggleOffhand.LOGGER.error("Failed to register key mapping in event: ", e);
+            // Set up the listener callback
+            java.util.function.Consumer<Object> handler = event -> {
+                try {
+                    if (ToggleOffhand.keyMapping == null) {
+                        ToggleOffhand.keyMapping = new KeyMapping(
+                                "key.toggle_offhand",
+                                InputConstants.Type.KEYSYM,
+                                GLFW.GLFW_KEY_UNKNOWN,
+                                KeyMapping.Category.MISC
+                        );
                     }
-                };
-                
-                // Register the listener via eventBus.addListener(EventPriority, Class, Consumer)
-                java.lang.reflect.Method addListenerMethod = eventBus.getClass().getMethod(
-                        "addListener", 
-                        priorityClass, 
-                        Class.class, 
-                        java.util.function.Consumer.class
-                );
-                addListenerMethod.invoke(eventBus, normalPriority, eventClass, handler);
-                ToggleOffhand.LOGGER.info("Successfully registered reflective listener callback for RegisterKeyMappingsEvent");
-            } else {
-                ToggleOffhand.LOGGER.error("Could not find mod container for toggle_offhand to get event bus");
-            }
+                    event.getClass().getMethod("register", KeyMapping.class).invoke(event, ToggleOffhand.keyMapping);
+                    ToggleOffhand.LOGGER.info("Registered keybind via Forge RegisterKeyMappingsEvent reflectively!");
+                } catch (Exception e) {
+                    ToggleOffhand.LOGGER.error("Failed to register key mapping in event: ", e);
+                }
+            };
+            
+            // Register the listener via eventBus.addListener(Consumer)
+            Class<?> eventBusClass = Class.forName("net.minecraftforge.eventbus.api.bus.EventBus");
+            java.lang.reflect.Method addListenerMethod = eventBusClass.getMethod(
+                    "addListener", 
+                    java.util.function.Consumer.class
+            );
+            addListenerMethod.invoke(eventBus, handler);
+            ToggleOffhand.LOGGER.info("Successfully registered reflective listener callback for RegisterKeyMappingsEvent");
         } catch (Exception e) {
             ToggleOffhand.LOGGER.error("Failed to register key mapping on Forge: ", e);
         }
